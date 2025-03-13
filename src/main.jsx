168ce0471a23819b3e1e011294f3a4d9b3d8e401
RetrozDev <ryanbarbozapro@gmail.com>
2025-03-13 15:52:37 +0100

    Added default react structure with biome js and routes

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+
+import "./reset.css"; // Optional: Reset browser styles
+// import "./index.css"; // Optional: Your global styles
+
+
+const router = createBrowserRouter([
+	{
+		element: <App />,
+		children: [
+			{ path: "/", element: <Home /> },
+			// Add more routes here
+		],
+	},
+]);
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+// Code structure based on the template proposal
+// 1. Base imports (React, Hooks, libraries ...)
+// 2. Tierce imports (components, assets, styles ...)
+// 3. Components
+// 4. Children components
+// 5. Utility and helper functions
+// 6. Styles and assets
+
+const Home = () => {
+	return (
+		<div>
+			<h1>Home</h1>
+            <p>Here you can edit this page </p>
+		</div>
+	);
+};
+export default Home;
